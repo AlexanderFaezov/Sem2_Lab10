d1c14c992dc91e504ddc90652ccaa310636d8453
AlexanderFaezov <s.faezov.com@mail.ru>
2024-03-05 18:03:04 +0500

    Добавьте файлы проекта.

---
+++ Sem2_Lab10/Sem2_Lab10.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void removeDuplicates(vector<string>& arr) {
+    sort(arr.begin(), arr.end());
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+}
+
+void removeLastK(vector<string>& arr, int k) {
+    if (k >= arr.size()) {
+        arr.clear();
+    }
+    else {
+        arr.erase(arr.end() - k, arr.end());
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    vector<string> strings = { "apple", "banana", "apple", "orange", "banana", "grape" };
+
+    cout << "Изначальный массив: " << endl;
+    for (const auto& str : strings) {
+        cout << str << " ";
+    }
+    cout << endl;
+
+    removeDuplicates(strings);
+
+    cout << "\nМассив после удаления повторов: " << endl;
+    for (const auto& str : strings) {
+        cout << str << " ";
+    }
+    cout << endl;
+
+    int k = 2;
+    removeLastK(strings, k);
+
+    cout << "\nМассив после удаления последних " << k << " элементов: " << endl;
+    for (const auto& str : strings) {
+        cout << str << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
